638b1d6c603f249220c767dc06292b9d861b173d
Louvre Chow <louvrecly@gmail.com>
2023-12-06 14:00:11 -0500

    create GenrePill component

---
+++ src/components/GenreList/GenrePill.tsx
@@ -0,0 +1,15 @@
+import Genre from '../../types/genre';
+
+interface GenrePillProps {
+  genre: Genre;
+}
+
+const GenrePill = ({ genre }: GenrePillProps) => {
+  return (
+    <span className="u-py-1 u-px-3 u-bg-indigo-600 u-rounded-full">
+      {genre}
+    </span>
+  );
+};
+
+export default GenrePill;
