68494755cc385f84744cf105a8ca85e2c71caad1
Louvre Chow <louvrecly@gmail.com>
2023-12-06 14:01:04 -0500

    create BookInfo component

---
+++ src/components/BookShelf/BookItem/BookInfo.tsx
@@ -0,0 +1,29 @@
+interface BookInfoProps {
+  title: string;
+  author: string;
+  price: number;
+  description: string;
+}
+
+const BookInfo = ({ title, author, price, description }: BookInfoProps) => {
+  return (
+    <>
+      <h2 className=" u-font-bold">{title}</h2>
+
+      <div className="u-text-sm u-flex u-justify-between u-gap-2">
+        <p>
+          <span>By </span>
+          <span className="u-font-bold">{author}</span>
+        </p>
+
+        <span className="u-font-bold">$&nbsp;{price}</span>
+      </div>
+
+      <div className="u-flex-grow">
+        <p className="u-text-xs u-italic u-line-clamp-4">{description}</p>
+      </div>
+    </>
+  );
+};
+
+export default BookInfo;
