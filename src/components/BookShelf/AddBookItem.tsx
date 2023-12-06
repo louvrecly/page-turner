e6ddf95c8ac3853095ba1444b441375811259c65
Louvre Chow <louvrecly@gmail.com>
2023-12-06 17:26:36 -0500

    dispatch toggleModal action in the BookItemContainer

--- src/components/BookShelf/AddBookItem.tsx
+++ src/components/BookShelf/AddBookItem.tsx
@@ -1,9 +1,16 @@
+import { useAppDispatch } from '../../hooks/redux';
+import { toggleModal } from '../../store/uiSlice';
 import BookItemContainer from './BookItem/Container';
 
 const AddBookItem = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <BookItemContainer className="u-from-sky-600/50 u-to-sky-400/50 u-transition-colors hover:u-from-sky-600/70 hover:u-to-sky-400/70">
-      <button className="u-absolute u-inset-0 u-bg-transparent u-text-3xl u-transition-transform hover:u-scale-150">
+      <button
+        className="u-absolute u-inset-0 u-text-3xl u-transition-transform hover:u-scale-150"
+        onClick={() => dispatch(toggleModal(true))}
+      >
         +
       </button>
     </BookItemContainer>
