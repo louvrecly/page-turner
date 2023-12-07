1b8213b1e53cc0266943a6058725a168592fc0a9
Louvre Chow <louvrecly@gmail.com>
2023-12-07 12:39:32 -0500

    wrap FormInput and FormTextArea with forwardRef

--- src/components/BookForm/FormInput.tsx
+++ src/components/BookForm/FormInput.tsx
@@ -1,19 +1,30 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import {
+  DetailedHTMLProps,
+  ForwardedRef,
+  InputHTMLAttributes,
+  forwardRef,
+} from 'react';
 import { twMerge } from 'tailwind-merge';
 
-const FormInput = ({
-  className,
-  ...props
-}: DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->) => {
-  return (
-    <input
-      className={twMerge('u-py-1 u-px-2 u-rounded', className)}
-      {...props}
-    />
-  );
-};
+const FormInput = forwardRef(
+  (
+    {
+      className,
+      ...props
+    }: DetailedHTMLProps<
+      InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <input
+        ref={ref}
+        className={twMerge('u-py-1 u-px-2 u-w-full u-rounded', className)}
+        {...props}
+      />
+    );
+  },
+);
 
 export default FormInput;
--- src/components/BookForm/FormTextArea.tsx
+++ src/components/BookForm/FormTextArea.tsx
@@ -1,19 +1,30 @@
-import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
+import {
+  DetailedHTMLProps,
+  ForwardedRef,
+  TextareaHTMLAttributes,
+  forwardRef,
+} from 'react';
 import { twMerge } from 'tailwind-merge';
 
-const FormTextArea = ({
-  className,
-  ...props
-}: DetailedHTMLProps<
-  TextareaHTMLAttributes<HTMLTextAreaElement>,
-  HTMLTextAreaElement
->) => {
-  return (
-    <textarea
-      className={twMerge('u-py-1 u-px-2 u-min-h-[32px] u-rounded', className)}
-      {...props}
-    />
-  );
-};
+const FormTextArea = forwardRef(
+  (
+    {
+      className,
+      ...props
+    }: DetailedHTMLProps<
+      TextareaHTMLAttributes<HTMLTextAreaElement>,
+      HTMLTextAreaElement
+    >,
+    ref: ForwardedRef<HTMLTextAreaElement>,
+  ) => {
+    return (
+      <textarea
+        ref={ref}
+        className={twMerge('u-py-1 u-px-2 u-min-h-[32px] u-rounded', className)}
+        {...props}
+      />
+    );
+  },
+);
 
 export default FormTextArea;
