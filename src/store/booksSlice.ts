673ddebfe0c457ce449d0c9601779d659a799600
Louvre Chow <louvrecly@gmail.com>
2023-12-07 12:37:17 -0500

    add maxBookId state to booksSlice and create selectMaxBookId

--- src/store/booksSlice.ts
+++ src/store/booksSlice.ts
@@ -3,10 +3,12 @@ import { RootState } from './store';
 import Book from '../types/book';
 
 interface BooksState {
+  maxBookId: number;
   books: Book[];
 }
 
 const initialState: BooksState = {
+  maxBookId: 0,
   books: [],
 };
 
@@ -16,12 +18,17 @@ export const booksSlice = createSlice({
   reducers: {
     setBooks(state, action: PayloadAction<Book[]>) {
       state.books = action.payload;
+      state.maxBookId = action.payload.reduce(
+        (maxId, book) => Math.max(maxId, book.id),
+        state.maxBookId,
+      );
     },
   },
 });
 
 export const { setBooks } = booksSlice.actions;
 export const selectBooks = (state: RootState) => state.books.books;
+export const selectMaxBookId = (state: RootState) => state.books.maxBookId;
 
 const booksReducer = booksSlice.reducer;
 
