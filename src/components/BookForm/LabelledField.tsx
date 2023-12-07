5fe346d3753d740dc9d1c98e8395f228072677f3
Louvre Chow <louvrecly@gmail.com>
2023-12-07 12:39:01 -0500

    add errorMessage prop to the LabelledField

--- src/components/BookForm/LabelledField.tsx
+++ src/components/BookForm/LabelledField.tsx
@@ -5,6 +5,7 @@ interface LabelledFieldProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   label: string;
   htmlFor?: string;
+  errorMessage?: string;
 }
 
 const LabelledField = ({
@@ -12,16 +13,18 @@ const LabelledField = ({
   children,
   htmlFor,
   className,
+  errorMessage = '',
 }: LabelledFieldProps) => {
   return (
-    <div
-      className={twMerge(
-        'u-flex u-justify-between u-items-center u-gap-2',
-        className,
-      )}
-    >
-      <label htmlFor={htmlFor}>{label}</label>
-      {children}
+    <div className={twMerge('u-flex u-justify-between u-gap-2', className)}>
+      <label className="u-py-1" htmlFor={htmlFor}>
+        {label}
+      </label>
+
+      <div className="u-basis-56 u-flex u-flex-col u-items-stretch u-gap-px">
+        {children}
+        <p className="u-w-full u-text-xs u-text-rose-600">{errorMessage}</p>
+      </div>
     </div>
   );
 };
