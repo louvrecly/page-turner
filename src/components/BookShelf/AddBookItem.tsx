b396df24ec10f2af9ab6d36cad23e8c9f72b1767
Louvre Chow <louvrecly@gmail.com>
2023-12-08 12:14:54 -0500

    adjust styling

--- src/components/BookShelf/AddBookItem.tsx
+++ src/components/BookShelf/AddBookItem.tsx
@@ -14,7 +14,7 @@ const AddBookItem = () => {
   }, [dispatch, maxBookId]);
 
   return (
-    <BookItemContainer className="u-from-sky-600/50 u-to-sky-400/50 u-transition-colors hover:u-from-sky-600/70 hover:u-to-sky-400/70">
+    <BookItemContainer className="u-from-sky-600/50 u-to-sky-400/50 u-transition-all hover:u-from-sky-600/70 hover:u-to-sky-400/70">
       <button
         className="u-absolute u-inset-0 u-text-3xl u-transition-transform hover:u-scale-150"
         onClick={handleClick}
