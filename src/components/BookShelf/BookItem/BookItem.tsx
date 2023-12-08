94838dae1848ec52bd81c31df3a186338d259f75
Louvre Chow <louvrecly@gmail.com>
2023-12-08 13:35:28 -0500

    adjust styling

--- src/components/BookShelf/BookItem/BookItem.tsx
+++ src/components/BookShelf/BookItem/BookItem.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useCallback } from 'react';
+import { MouseEvent, useCallback, useMemo } from 'react';
 import BookItemContainer from './Container';
 import BookInfo from './BookInfo';
 import GenreList from '../../GenreList';
@@ -6,6 +6,23 @@ import { useAppDispatch } from '../../../hooks/redux';
 import { toggleModal } from '../../../store/uiSlice';
 import { setBookForm } from '../../../store/booksSlice';
 import Book from '../../../types/book';
+
+const GRADIENT_COLORS = [
+  'u-from-rose-900 u-to-rose-600',
+  'u-from-red-900 u-to-red-600',
+  'u-from-orange-900 u-to-orange-600',
+  'u-from-amber-900 u-to-amber-600',
+  'u-from-yellow-900 u-to-yellow-600',
+  'u-from-lime-900 u-to-lime-600',
+  'u-from-green-900 u-to-green-600',
+  'u-from-emerald-900 u-to-emerald-600',
+  'u-from-teal-900 u-to-teal-600',
+  'u-from-cyan-900 u-to-cyan-600',
+  'u-from-blue-900 u-to-blue-600',
+  'u-from-purple-900 u-to-purple-600',
+  'u-from-fuchsia-900 u-to-fuchsia-600',
+  'u-from-pink-900 u-to-pink-600',
+];
 
 interface BookItemProps {
   book: Book;
@@ -28,8 +45,13 @@ const BookItem = ({ book }: BookItemProps) => {
     [book.id, dispatch],
   );
 
+  const gradientColor = useMemo(
+    () => GRADIENT_COLORS[book.id % GRADIENT_COLORS.length],
+    [book.id],
+  );
+
   return (
-    <BookItemContainer onClick={handleBookOnClick}>
+    <BookItemContainer className={gradientColor} onClick={handleBookOnClick}>
       <button
         className="u-absolute u-top-px u-right-2"
         onClick={handleCloseButtonOnClick}
--- src/components/BookShelf/BookItem/Container.tsx
+++ src/components/BookShelf/BookItem/Container.tsx
@@ -14,7 +14,7 @@ const BookItemContainer = ({
   return (
     <div
       className={twMerge(
-        'u-relative u-py-4 u-px-6 u-flex-grow u-aspect-[2/3] u-bg-gradient-to-b u-from-emerald-900 u-to-emerald-600 u-rounded u-shadow u-flex u-flex-col u-items-stretch u-gap-2 u-cursor-pointer u-transition-transform sm:u-flex-grow-0 sm:u-basis-1/3 md:u-basis-1/4 lg:u-basis-1/5 hover:u-scale-105',
+        'u-relative u-p-4 u-flex-grow u-aspect-[2/3] u-bg-gradient-to-b u-from-emerald-900 u-to-emerald-600 u-rounded u-shadow u-flex u-flex-col u-items-stretch u-gap-2 u-cursor-pointer u-transition-transform sm:u-flex-grow-0 sm:u-basis-1/3 md:u-basis-1/4 lg:u-basis-1/5 hover:u-scale-105',
         className,
       )}
       {...props}
